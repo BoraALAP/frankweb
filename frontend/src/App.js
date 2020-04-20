b2d0bf2a0cf062eb99f5748f260784a29d14c64b
BoraALAP <alapbora@gmail.com>
2020-04-20 12:28:25 -0400

    production ready

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,23 +11,6 @@ import GlobalStyle from "./styles/global";
 import Layout from "./components/global/Layout";
 import DoorApplication from "./pages/DoorApplication";
 
-import CamingOption from "./dbComponents/CamingOption";
-import GlassAssociation from "./dbComponents/GlassAssociation";
-import DividedLiteType from "./dbComponents/DividedLiteType";
-import DoorCollections from "./dbComponents/DoorCollections";
-import DoorLine from "./dbComponents/DoorLine";
-import Doors from "./dbComponents/Doors";
-import Finishes from "./dbComponents/Finishes";
-import FrameProfile from "./dbComponents/FrameProfile";
-import Glass from "./dbComponents/Glass";
-import GlassFamily from "./dbComponents/GlassFamily";
-import GlassFeature from "./dbComponents/GlassFeature";
-import GrilleColor from "./dbComponents/GrilleColor";
-import HandleSets from "./dbComponents/HandleSets";
-import Sidelites from "./dbComponents/Sidelites";
-import StyleLayoutPairs from "./dbComponents/StyleLayoutPairs";
-import StyleShapes from "./dbComponents/StyleShapes";
-import Transoms from "./dbComponents/Transoms";
 import DisplayAll from "./components/applicationComponents/DisplayAll";
 
 const App = () => {
@@ -41,24 +24,8 @@ const App = () => {
           <Layout>
             <Switch>
               <Route path="/application" component={DoorApplication} />
-              <Route path="/" exact component={DisplayAll}>
-                {/* <GlassAssociation /> */}
-                {/* <FrameProfile /> */}
-                {/* <GlassFeature /> */}
-                {/* <GlassFamily /> */}
-                {/* <CamingOption /> */}
-                {/* <DividedLiteType /> */}
-                {/* <GrilleColor /> */}
-                {/* <StyleLayoutPairs /> */}
-                {/* <Finishes /> */}
-                {/* <HandleSets /> */}
-                {/* <StyleShapes /> */}
-                {/* <DoorCollections /> */}
-                {/* <DoorLine /> */}
-                {/* <Glass /> */}
-                {/* <Transoms /> */}
-                {/* <Sidelites /> */}
-                {/* <Doors /> */}
+              <Route path="/" exact>
+                <DisplayAll />
               </Route>
             </Switch>
           </Layout>
